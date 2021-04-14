71b131d34a4bf99102901c49708acf048ede96a9
EnriqueLunaGuzman <enrique.luna@ejamerica.com>
2021-04-13 17:40:51 -0700

    Code of 13/04/2021

---
+++ my-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render( ) {
+    return (
+      <div className="ui internally celled grid">
+        <div className="row">
+          <div className="three wide column">
+
+          </div>
+          <div className="ten wide column">
+            <h1>Home Page</h1>
+          </div>
+          <div className="three wide column">
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="three wide column">
+
+          </div>
+          <div className="ten wide column">
+            <p>Welcome to Home!</p>
+          </div>
+          <div className="three wide column">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
