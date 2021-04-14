24e77bc31654719b73cfbc47258e4e600b96086b
EnriqueLunaGuzman <enrique.luna@ejamerica.com>
2021-04-14 12:30:11 -0700

    Code of 14/04/2021

---
+++ my-app/src/containers/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import MasterLayout from '../layouts/MasterLayout';
+import HomePage from '../containers/HomePage/HomePage';
+
+class App extends Component {
+
+  render( ) {
+    return (
+      <MasterLayout>
+        <HomePage />
+      </MasterLayout>
+    );
+  }
+}
+
+export default App;
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-import 'fomantic-ui-css/semantic.min.css';
+import App from './containers/App';
+import '@fontsource/roboto';
 
 ReactDOM.render( <React.StrictMode><App /></React.StrictMode>, document.getElementById('root') );
